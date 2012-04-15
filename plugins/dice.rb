9700ecd7f8aae23d24a9b3e1fe5ab9d15b2722a2
Daniel Fairley <poemdexter@gmail.com>
2012-04-15 13:54:09 -0500

    fixed dice roller plugin

--- plugins/dice.rb
+++ plugins/dice.rb
@@ -1,9 +1,18 @@
 class Dice
   include Cinch::Plugin
 
-  match /roll/, :method => :roll
-
-  def roll(m)
-    m.reply "You rolled", true
+  listen_to :message
+  
+  def listen(m)
+    dice_regex = /!roll\s(?<d1>\d+)d(?<d2>\d+)(\z|\+(?<d3>\d+))?/
+    match = dice_regex.match(m.message)
+    if match
+      m.reply "You rolled #{roll(match[:d1],match[:d2],match[:d3])}", true
+    end
+  end
+  
+  def roll(d1,d2,d3)
+    result = d1.to_i * (1 + rand(d2.to_i))
+    result = result + d3.to_i if (d3.to_i)
   end
 end
