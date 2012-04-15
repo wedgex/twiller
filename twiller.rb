6eb96c86b0d6fb357a8c753c54ef2cc68c1df9ca
Daniel Fairley <poemdexter@gmail.com>
2012-04-15 16:52:47 -0500

    added spotify url plugin

--- twiller.rb
+++ twiller.rb
@@ -1,13 +1,16 @@
 require 'cinch'
+require 'nokogiri'
+require 'open-uri'
 require_relative 'plugins/auto_op'
 require_relative 'plugins/dice'
+require_relative 'plugins/spotify'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'irc.freenode.net'
     c.channels = ['#twillers']
-    c.nick = 'twiller'
-    c.plugins.plugins = [AutoOp, Dice]
+    c.nick = 'twillbot'
+    c.plugins.plugins = [AutoOp, Dice, Spotify]
   end
 end
 
