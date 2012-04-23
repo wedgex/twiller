1d94fb47166b791d179b1b012aa948991e271355
Daniel Fairley <poemdexter@gmail.com>
2012-04-22 21:15:13 -0500

    added time left to diablo plugin

--- plugins/diablo.rb
+++ plugins/diablo.rb
@@ -6,10 +6,14 @@ class Diablo
     rx = /d(iablo)?[ ]?(3|iii)? out yet/i
 
     if m.message.match rx
-      release = Time.local(2012, 5, 15)
-      # Local time had better be right!
-      reply = (Time.new >= release) ? 'Yesssssssss' : 'Nope.'
-      m.reply reply
+      release = Time.mktime(2012,5,15,0,0,0,0)
+      if Time.now >= release
+        m.reply "Oh my god, let\'s play Diablo III!"
+      else
+        secs = release - Time.now
+        time_left = "%d days, %d hours, %d minutes, and %d seconds" % [ secs.to_i / 86400, (secs % 86400).to_i / 3600, *((secs % 3600).divmod(60)) ]
+        m.reply "No, you still have #{time_left} left."
+      end
     end
   end
 end
