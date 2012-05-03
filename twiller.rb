73b5672b47bbf53d1ea805ea5f522f72c930644f
Adam Clarke <adamclarkewastaken+github@gmail.com>
2012-05-03 00:52:31 -0500

    COMPUTER LOAD UP CELERY MAN

--- twiller.rb
+++ twiller.rb
@@ -11,14 +11,15 @@ require_relative 'plugins/youtube'
 require_relative 'plugins/diablo'
 require_relative 'plugins/interesting'
 require_relative 'plugins/markovgen'
+require_relative 'plugins/celery_man'
 require_relative 'lib/markov'
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = 'irc.freenode.net'
     c.channels = Twiller::Config['channels'].keys
-    c.nick = 'twillbot'
-    c.plugins.plugins = [AutoOp, Dice, Spotify, Youtube, Butts, Diablo, Interesting, Markovgen]
+    c.nick = 'dumbot'
+    c.plugins.plugins = [AutoOp, Dice, Spotify, Youtube, Butts, Diablo, Interesting, Markovgen, CeleryMan]
   end
 end
 
